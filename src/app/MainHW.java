09782d1bab8e8772952835d60a607c8ebba7a46d
InnaDudka <innadudka1@gmail.com>
2024-09-28 23:23:36 +0300

    first commit

---
+++ src/app/MainHW.java
@@ -0,0 +1,33 @@
+package app;
+
+public class MainHW {
+    public static void main(String[] args) {
+
+        String finishSymbol = ".";
+        String product = "Product No ";
+        int prodNum1 = 1;
+        String smart = ": smartphone,";
+        String total = "total sales for ";
+        int dayNum1 = 5;
+        String days = " days ";
+        String is = " is EUR ";
+        String sales ="sales by day";
+        double sum = 12153.41;
+        double divide = sum / dayNum1;
+        System.out.println(product + prodNum1 + smart + "\n" +
+                total + dayNum1 + days + is + sum + "\n" +
+                sales + is + divide + finishSymbol);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
